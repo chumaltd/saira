f066ff22f309f1e81f475ef6d169317b7ab7ca80
Chuma Takahiro <chuma@unicorn.limited>
2025-04-30 10:28:40 +0900

    add width:stretch to .container

--- lit/container.js
+++ lit/container.js
@@ -1,4 +1,4 @@
 import {css} from 'lit';
 export default css`
-.container-sm{max-width:544px;margin-right:auto;margin-left:auto}.container-md{max-width:min(768px,100vw);margin-right:auto;margin-left:auto}.container-lg{max-width:min(1024px,100vw);margin-right:auto;margin-left:auto}.container-xl{max-width:min(1216px,100vw);margin-right:auto;margin-left:auto}
+.container-sm{width:-webkit-fill-available;width:-moz-available;width:stretch;max-width:544px;margin-right:auto;margin-left:auto}.container-md{width:-webkit-fill-available;width:-moz-available;width:stretch;max-width:min(768px,100vw);margin-right:auto;margin-left:auto}.container-lg{width:-webkit-fill-available;width:-moz-available;width:stretch;max-width:min(1024px,100vw);margin-right:auto;margin-left:auto}.container-xl{width:-webkit-fill-available;width:-moz-available;width:stretch;max-width:min(1216px,100vw);margin-right:auto;margin-left:auto}
 `
