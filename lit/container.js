9f694507c9398ba0bdf1d47c79a83cf26898c15a
Chuma Takahiro <chuma@unicorn.limited>
2026-08-07 10:36:36 +0900

    change smartphone layout upto default 640px

--- lit/container.js
+++ lit/container.js
@@ -1,4 +1,4 @@
 import {css} from 'lit';
 export default css`
-.container-sm{width:-webkit-fill-available;width:-moz-available;width:stretch;max-width:544px;margin-right:auto;margin-left:auto}.container-md{width:-webkit-fill-available;width:-moz-available;width:stretch;max-width:min(768px,100vw);margin-right:auto;margin-left:auto}.container-lg{width:-webkit-fill-available;width:-moz-available;width:stretch;max-width:min(1024px,100vw);margin-right:auto;margin-left:auto}.container-xl{width:-webkit-fill-available;width:-moz-available;width:stretch;max-width:min(1216px,100vw);margin-right:auto;margin-left:auto}
+.container-sm{width:-webkit-fill-available;width:-moz-available;width:stretch;max-width:640px;margin-right:auto;margin-left:auto}.container-md{width:-webkit-fill-available;width:-moz-available;width:stretch;max-width:min(768px,100vw);margin-right:auto;margin-left:auto}.container-lg{width:-webkit-fill-available;width:-moz-available;width:stretch;max-width:min(1024px,100vw);margin-right:auto;margin-left:auto}.container-xl{width:-webkit-fill-available;width:-moz-available;width:stretch;max-width:min(1216px,100vw);margin-right:auto;margin-left:auto}
 `
